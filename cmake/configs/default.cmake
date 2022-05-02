909559308495bf3ff6cb038e99a6b654cb52e0f5
Your Name <sasha.nov0707@gmail.com>
2022-05-02 21:50:01 +0300

    ~

--- cmake/configs/default.cmake
+++ cmake/configs/default.cmake
@@ -455,6 +455,7 @@ hunter_default_version(pluginlib VERSION 1.12.1-p0)
 hunter_default_version(poly2tri VERSION 1.0.0)
 hunter_default_version(polyclipping VERSION 4.8.8-p0) # for Assimp
 hunter_default_version(presentproto VERSION 1.0)
+hunter_default_version(print VERSION v1)
 hunter_default_version(prometheus-cpp VERSION 0.6.0-p2)
 hunter_default_version(protobuf-c VERSION 1.3.0-p1)
 hunter_default_version(pthread-stubs VERSION 0.4)
